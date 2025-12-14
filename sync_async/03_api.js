fbe1b42c043e6dd3f1e2ac943fc3e819f1284f76
shionsuio <s1f10230230f@iniad.org>
2025-12-14 16:28:13 +0900

    エラーハンドリング強化

--- sync_async/03_api.js
+++ sync_async/03_api.js
@@ -1,17 +1,22 @@
-const resaloveAPI  = () => {
-    return new Promise((resolve, reject) => {
-        fetch('https://jsonplaceholder.typicode.com//posts')
-        .then(res => res.json())
-        .then(data => resolve(data))
-        .catch(err => reject(err));
-    });
+const resolveAPI = () => {
+  return new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        if (!res.ok) {
+          reject(new Error('HTTP error'));
+        }
+        return res.json();
+      })
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+  });
 };
 
 const asyncAPICall = async () => {
     console.log('calling API');
     try {
-        const result = await resaloveAPI()
-        console.log(result);
+        const result = await resolveAPI()
+        console.log(result[0].userId);
     } catch (error) {
         console.log('Error:', error);
     }
